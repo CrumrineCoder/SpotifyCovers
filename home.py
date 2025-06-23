5173e0a4e00a9cedab03cbf1c6eaa873a81bb987
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-06-22 20:08:45 -0500

    Environment variables - spotify get a user's liked songs

--- home.py
+++ home.py
@@ -1,5 +1,54 @@
-from flask import Flask
+from flask import Flask, redirect, request, session, url_for
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI = os.environ.get('SPOTIPY_REDIRECT_URI')
+
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.secret_key = os.urandom(24)
+
+SCOPE = 'user-library-read'
+
+@app.route('/')
+def index():
+    return redirect("/login")
+
+@app.route('/login')
+def login():
+    sp_oauth = SpotifyOAuth(
+        SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope=SCOPE
+    )
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    sp_oauth = SpotifyOAuth(
+        SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope=SCOPE
+    )
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session['token_info'] = token_info
+    return redirect(url_for('liked_songs'))
+
+@app.route('/liked-songs')
+def liked_songs():
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect(url_for('login'))
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    results = sp.current_user_saved_tracks(limit=50)
+    songs = [
+        {
+            'name': item['track']['name'],
+            'artist': item['track']['artists'][0]['name']
+        }
+        for item in results['items']
+    ]
+    return {'liked_songs': songs}
